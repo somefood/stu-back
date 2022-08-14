58ed2863cbdfde9df2d71d6a0de6b2e3208b4c29
xi_jjun <rlawowns97@naver.com>
2022-08-15 01:56:22 +0900

    feat: add Broadcast, Category entity

---
+++ src/main/java/com/stu/backend/domain/Category.java
@@ -0,0 +1,24 @@
+package com.stu.backend.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(targetEntity = Broadcast.class, fetch = FetchType.LAZY)
+	@JoinColumn(name = "broadcast_id")
+	private Broadcast broadcast;
+
+	@ManyToOne(targetEntity = Tag.class, fetch = FetchType.LAZY)
+	@JoinColumn(name = "tag_id")
+	private Tag tag;
+}
