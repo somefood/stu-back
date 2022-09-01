cb6431a0e8a377e56a16ebf8ffc5a9f09b68b7fa
Seokju Hong <iamseokjuhong@gmail.com>
2022-09-01 19:53:17 +0900

    refactor: BaseEntity를 도입하여 create, update 필드 공통화

--- src/main/java/com/stu/backend/domain/Broadcast.java
+++ src/main/java/com/stu/backend/domain/Broadcast.java
@@ -2,23 +2,18 @@ package com.stu.backend.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Entity
-public class Broadcast {
+public class Broadcast extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -32,11 +27,11 @@ public class Broadcast {
 
 	private String imageUrl;
 
-	private LocalDateTime startDateTime;
-
-	private LocalDateTime endDateTime;
-
 	@ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id")
 	private User user;
+
+	public void setUser(User user) {
+		this.user = user;
+	}
 }
